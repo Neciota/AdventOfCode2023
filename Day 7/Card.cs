9bed708fd97348ddf8bbc69f9385fcdedaaab75d
Rik van den berg <rvdberg-@hotmail.com>
2023-12-09 14:55:40 +0100

    Day 7 Part 2

--- Day 7/Card.cs
+++ Day 7/Card.cs
@@ -2,18 +2,22 @@
 {
     internal enum Card
     {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Queen = 12,
+        King = 13,
+        Ace = 14,
+#if PART_2
+        Jack = 1
+#else
+        Jack = 11
+#endif
     }
 }
--- Day 7/Hand.cs
+++ Day 7/Hand.cs
@@ -11,22 +11,21 @@
 
             Cards = cards.ToArray();
             Bet = bet;
+            Rank = GetHandRank();
         }
 
         public Card[] Cards { get; private set; }
         public int Bet { get; private set; }
-        public int Rank { get; set; }
+        public HandRank Rank { get; private set; }
 
         public int CompareTo(Hand? other)
         {
             if (other is null)
                 return 1;
 
-            HandRank ownRank = GetHandRank();
-            HandRank otherRank = other.GetHandRank();
-            if (ownRank > otherRank)
+            if (Rank > other.Rank)
                 return 1;
-            else if (ownRank < otherRank)
+            else if (Rank < other.Rank)
                 return -1;
 
             // High card rules
@@ -43,28 +42,72 @@
 
         public HandRank GetHandRank()
         {
-            int cardSize = Enum.GetValues(typeof(Card)).Length;
-            int[] cardCounts = new int[cardSize];
-            
+            int[] cardCounts = new int[15];
+
             foreach (Card card in Cards)
             {
                 cardCounts[(int)card]++;
             }
 
-            if (Array.Exists(cardCounts, x => x == 5))
-                return HandRank.FiveOfAKind;
-            if (Array.Exists(cardCounts, x => x == 4))
-                return HandRank.FourOfAKind;
-            if (Array.Exists(cardCounts, x => x == 3) && Array.Exists(cardCounts, x => x == 2))
-                return HandRank.FullHouse;
-            if (Array.Exists(cardCounts, x => x == 3))
-                return HandRank.ThreeOfAKind;
-            if (cardCounts.Count(x => x == 2) == 2)
-                return HandRank.TwoPair;
-            if (cardCounts.Count(x => x == 2) == 1)
-                return HandRank.OnePair;
+#if PART_2
+            int jackCounts = cardCounts[(int)Card.Jack];
+            cardCounts = cardCounts.Skip(2).ToArray();
+#endif
 
-            return HandRank.HighCard;
+            HandRank handRank = HandRank.HighCard;
+            if (Array.Exists(cardCounts, x => x == 5))
+                handRank = HandRank.FiveOfAKind;
+            else if (Array.Exists(cardCounts, x => x == 4))
+                handRank = HandRank.FourOfAKind;
+            else if (Array.Exists(cardCounts, x => x == 3) && Array.Exists(cardCounts, x => x == 2))
+                handRank = HandRank.FullHouse;
+            else if (Array.Exists(cardCounts, x => x == 3))
+                handRank = HandRank.ThreeOfAKind;
+            else if (cardCounts.Count(x => x == 2) == 2)
+                handRank = HandRank.TwoPair;
+            else if (cardCounts.Count(x => x == 2) == 1)
+                handRank = HandRank.OnePair;
+
+#if PART_2
+            switch (handRank)
+            {
+                case HandRank.FourOfAKind:
+                    if (jackCounts == 1)
+                        handRank = HandRank.FiveOfAKind;
+                    break;
+                case HandRank.ThreeOfAKind:
+                    if (jackCounts == 2)
+                        handRank = HandRank.FiveOfAKind;
+                    else if (jackCounts == 1)
+                        handRank = HandRank.FourOfAKind;
+                    break;
+                case HandRank.TwoPair:
+                    if (jackCounts == 1)
+                        handRank = HandRank.FullHouse;
+                    break;
+                case HandRank.OnePair:
+                    if (jackCounts == 3)
+                        handRank = HandRank.FiveOfAKind;
+                    else if (jackCounts == 2)
+                        handRank = HandRank.FourOfAKind;
+                    else if (jackCounts == 1)
+                        handRank = HandRank.ThreeOfAKind;
+                    break;
+                case HandRank.HighCard:
+                    if (jackCounts == 5)
+                        handRank = HandRank.FiveOfAKind;
+                    else if (jackCounts == 4)
+                        handRank = HandRank.FiveOfAKind;
+                    else if (jackCounts == 3)
+                        handRank = HandRank.FourOfAKind;
+                    else if (jackCounts == 2)
+                        handRank = HandRank.ThreeOfAKind;
+                    else if (jackCounts == 1)
+                        handRank = HandRank.OnePair;
+                    break;
+            }
+#endif
+            return handRank;
         }
 
         public override string ToString()
