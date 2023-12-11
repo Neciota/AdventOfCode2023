5f0df3e0715f22d5a7e7d889755d88814a483911
Rik van den berg <rvdberg-@hotmail.com>
2023-12-11 11:21:31 +0100

    Day 9 Part 1

---
+++ Day 9/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day_9;
+
+string[] input = File.ReadAllLines("input.txt");
+long[][] serieses = input.Select(x => x.Split(' ').Select(y => Convert.ToInt64(y)).ToArray()).ToArray();
+
+long result = serieses.Select(SeriesAnalyzer.GetNextValue).Sum();
+Console.WriteLine(result);
---
+++ Day 9/SeriesAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_9
+{
+    internal static class SeriesAnalyzer
+    {
+        public static long GetNextValue(IEnumerable<long> series)
+        {
+            IEnumerable<long> differences = series.SkipLast(1).Zip(series.Skip(1)).Select(x => x.Second - x.First);
+
+            long nextValue = differences.Any(x => x != 0) ? GetNextValue(differences) : 0;
+
+            return series.Last() + nextValue;
+        }
+    }
+}
